cdc06490dd1354443292150fd2d15369c4386982
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2024-03-31 12:23:07 +0530

    Update LoginTest.java
    Changes in whole code as I have created separate file for LoginTestDataDriven

--- src/test/java/com/itlearn/testcases/LoginTest.java
+++ src/test/java/com/itlearn/testcases/LoginTest.java
@@ -1,36 +1,29 @@
 package com.itlearn.testcases;
 
 import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
 import java.io.IOException;
-
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
 import com.itlearn.pages.BaseTest;
 import com.itlearn.pages.LoginPage;
-import com.itlearn.utility.ReadExcelFile;
 
 public class LoginTest extends BaseTest {
 	
-	String fileName = System.getProperty("user.dir")+"\\TestData\\testInfo.xlsx";
+	//String fileName = System.getProperty("user.dir")+"\\TestData\\testInfo.xlsx";
 	
-	@Test(priority =1,dataProvider="LoginDataProvider")
-	void verifyLogin(String userEmail,String userPwd) throws IOException{
+	@Test
+	void verifyLogin() throws IOException{
 		
 		//We are extending BaseTest class so we have to pass driver in LoginPage object.
 		//Once the value is passed to loginpage so it allows you to perform some task inside the loginpage i.e is help to automate the web element here 
-		LoginPage lp = new LoginPage(driver);
-		//String username ="Demo12";
-		//String password = "Test123456$";
-		lp.loginPortal(userEmail, userPwd);
+		LoginPage lp=new LoginPage(driver);
+		String username ="Demo12";
+		String password ="Test123456$";
+		lp.loginPortal(username, password);
 		
-		if(userEmail.equals("Demo12") && userPwd.equals("Test123456$"))
+		if(username.equals("Demo12") && password.equals("Test123456$"))
 		{
 			System.out.println("Test Passed");
 			Assert.assertTrue(true);
-			lp.logout(); //there is no login button to do login again
 		}
 		else
 		{
@@ -38,25 +31,4 @@ public class LoginTest extends BaseTest {
 			Assert.assertTrue(false);
 		}
 	}
-	
-	@DataProvider(name="LoginDataProvider")
-	public String[][] LoginDataProvider()
-	{
-		
-		
-		int ttlRows= ReadExcelFile.getRowCount(fileName, "LoginData");
-		int ttlColumns= ReadExcelFile.getColCount(fileName, "LoginData");
-		
-		String data[][]= new String[ttlRows-1][ttlColumns];
-		
-		for(int i=1;i<ttlRows;i++)
-		{
-			for(int j=0;j<ttlColumns;j++)
-			{
-				data[i-1][j]=ReadExcelFile.getCellValue(fileName, "LoginData", i, j);
-			}
-		}
-		return data;
-	}
-
 }
