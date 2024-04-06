083929eb036c045bc0268b7f4b8fe528edb248c1
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2024-04-06 09:21:19 +0530

    Update SearchTestCase.java
    Comment login line of code

--- src/test/java/com/itlearn/testcases/SearchTestCase.java
+++ src/test/java/com/itlearn/testcases/SearchTestCase.java
@@ -11,12 +11,12 @@ public class SearchTestCase extends BaseTest{
 	String fileName=System.getProperty("user.dir")+"\\TestData\\TestPortalData.xlsx";
 	
 	@Test(priority =1)
-	void searchCourseTesr()
+	void searchCourseTest()
 	{
-		LoginPage lp=new LoginPage(driver);
-		String username=ReadExcelFile.getCellValue(fileName, "LoginData", 1, 0);
-		String password=ReadExcelFile.getCellValue(fileName, "LoginData", 1, 1);
-		lp.loginPortal(username, password);
+		//LoginPage lp=new LoginPage(driver);
+		//String username=ReadExcelFile.getCellValue(fileName, "LoginData", 1, 0);
+		//String password=ReadExcelFile.getCellValue(fileName, "LoginData", 1, 1);
+		//lp.loginPortal(username, password);
 		
 		SearchPage sp= new SearchPage(driver);
 		String serachCourse=ReadExcelFile.getCellValue(fileName, "SearchCourse", 0, 0);
